714cff47adedcc633ae89d0879fd2da9683f1a27
vinayak0127 <vinayakrajbusiness@gmail.com>
2021-03-10 00:55:06 +0530

    test

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.min.css'; import
 'bootstrap-css-only/css/bootstrap.min.css'; import
 'mdbreact/dist/css/mdb.css';
+import bodyParser from 'body-parser';
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
